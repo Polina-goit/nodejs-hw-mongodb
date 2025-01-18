4446f06944b5505e125cdff4884c1b9de4b269bf
Polina <polinakoctyu4enko@gmail.com>
2025-01-18 18:18:41 +0200

    step-2

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,23 +1,30 @@
 import { getAllContacts, getContactById } from "../services/contacts.js";
+import createHttpError from "http-errors";
+export const getContactsController = async (req, res, next) => {
+    try {
+        const contacts = await getAllContacts();
 
-export const getContactsController = async (req, res) => {
-    const contacts = await getAllContacts();
-
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
+        res.json({
+            status: 200,
+            message: 'Successfully found contacts!',
+            data: contacts,
+        });
+    } catch (err) {
+        next(err);
+    }
 };
-export const getContactByIdController = async (req, res) => {
+export const getContactByIdController = async (req, res, next) => {
      const { contactId } = req.params;
     const contact = await getContactById(contactId);
+    // if (!contact) {
+    //   res.status(404).json({
+    //     message: 'Contact not found',
+    //   });
+    //   return;
+    // }
     if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found',
-      });
-      return;
-    }
+       throw createHttpError(404, 'Contact not found');
+            }
     res.json({
       status: 200,
       message: `Successfully found contact with id ${contactId}!`,
--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
 import contactsRouter from './routers/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 
 const PORT = Number(getEnvVar('PORT', '3000'));
@@ -19,12 +21,9 @@ export const setupServer = () => {
     }),
   );
   app.use(contactsRouter);
-  
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
+
+  app.use('*', notFoundHandler);
+  app.use(errorHandler);
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
