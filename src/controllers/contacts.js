a471cf385ab39271c370b373d3ba627219cbfee0
Polina <polinakoctyu4enko@gmail.com>
2025-01-22 21:43:05 +0200

    step 3

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,8 +1,13 @@
 import { createContact, deleteContact, getAllContacts, getContactById, updateContact } from "../services/contacts.js";
 import createHttpError from "http-errors";
+import { parsePaginationParams } from "../utils/parsePaginationParams.js";
 export const getContactsController = async (req, res, next) => {
-    try {
-        const contacts = await getAllContacts();
+  try {
+        const { page, perPage } = parsePaginationParams(req.query);
+        const contacts = await getAllContacts({
+    page,
+    perPage,
+  });
 
         res.json({
             status: 200,
