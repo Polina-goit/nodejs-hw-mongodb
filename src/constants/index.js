bd7febd2dfd3f37612e24ee57eb040d83bb86762
Polina <polinakoctyu4enko@gmail.com>
2025-01-25 17:24:17 +0200

    HW-5-1

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ export const SORT_ORDER = {
   DESC: 'desc',
 };
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
-export const ONE_DAY = 24 * 60 * 60 * 1000;
+export const ONE_DAY = 30 * 24 * 60 * 60 * 1000;
 export const ROLES = {
   TEACHER: 'teacher',
   PARENT: 'parent',
