8e55349e9a619499cbe61932786d878554398747
Polina <polinakoctyu4enko@gmail.com>
2025-02-11 20:54:32 +0200

    reset email 2

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -79,8 +79,8 @@ export const logoutUserController = async (req, res) => {
 export const sendResetEmailController = async (req, res) => {
   await requestResetToken(req.body.email);
   res.json({
-    message: 'Reset password email was successfully sent!',
     status: 200,
-    data: {},
+       message: "Reset password email has been successfully sent.",
+       data: {},
   });
 };
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,11 +1,14 @@
 import bcrypt from 'bcrypt';
 import { randomBytes } from 'crypto';
 import jwt from 'jsonwebtoken';
+import handlebars from 'handlebars';
+import path from 'node:path';
+import fs from 'node:fs/promises';
 
 import { UsersCollection } from '../db/models/user.js';
 import createHttpError from 'http-errors';
 import { SessionsCollection } from '../db/models/session.js';
-import { FIFTEEN_MINUTES, ONE_DAY, SMTP } from '../constants/index.js';
+import { FIFTEEN_MINUTES, ONE_DAY, SMTP, TEMPLATES_DIR } from '../constants/index.js';
 import { getEnvVar } from '../utils/getEnvVar.js';
 import { sendEmail } from '../utils/sendMail.js';
 
@@ -92,7 +95,7 @@ export const logoutUser = async (sessionId) => {
 export const requestResetToken = async (email) => {
   const user = await UsersCollection.findOne({ email });
   if (!user) {
-    throw createHttpError(404, 'User not found');
+    throw createHttpError(404, 'User not found!');
   }
   const resetToken = jwt.sign(
     {
@@ -104,11 +107,31 @@ export const requestResetToken = async (email) => {
       expiresIn: '5m',
     },
   );
+  const resetPasswordTemplatePath = path.join(
+    TEMPLATES_DIR,
+    'reset-password-email.html',
+  );
 
+  const templateSource = (
+    await fs.readFile(resetPasswordTemplatePath)
+  ).toString();
+
+  const template = handlebars.compile(templateSource);
+  const html = template({
+    name: user.name,
+    link: `${getEnvVar('APP_DOMAIN')}/reset-password?token=${resetToken}`,
+  });
+try {
   await sendEmail({
-    from: getEnvVar (SMTP.SMTP_FROM),
+    from: getEnvVar(SMTP.SMTP_FROM),
     to: email,
     subject: 'Reset your password',
-    html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
+    html,
   });
-  };
+} catch (error) {
+  console.log(error);
+    throw createHttpError(
+      500,
+      'Failed to send the email, please try again later.',
+    );}
+};
