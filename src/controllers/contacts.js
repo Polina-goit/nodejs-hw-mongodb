a9cc10918948795bea39630f55cd7107d4a828ab
Polina <polinakoctyu4enko@gmail.com>
2025-02-11 23:07:57 +0200

    update 1

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -113,7 +113,7 @@ export const patchContactController = async (req, res, next) => {
   });
 
   if (!result) {
-    next(createHttpError(404, 'Contact not found'));
+    next(createHttpError(404, 'Contact not found!'));
     return;
   }
 
@@ -134,3 +134,4 @@ export const deleteContactController = async (req, res, next) => {
   }
   res.status(204).send();
 };
+
