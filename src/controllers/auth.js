656adf4d839d4d58f857b5866b9edc0885da9d3c
Polina <polinakoctyu4enko@gmail.com>
2025-02-13 22:18:46 +0200

    step 1

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,6 @@
 import { ONE_DAY } from '../constants/index.js';
 import {
+  loginOrSignupWithGoogle,
   loginUser,
   logoutUser,
   refreshUsersSession,
@@ -7,6 +8,7 @@ import {
   requestResetToken,
   resetPassword,
 } from '../services/auth.js';
+import { generateAuthUrl } from '../utils/googleOAuth2.js';
 
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
@@ -81,8 +83,8 @@ export const sendResetEmailController = async (req, res) => {
   await requestResetToken(req.body.email);
   res.json({
     status: 200,
-       message: "Reset password email has been successfully sent.",
-       data: {},
+    message: 'Reset password email has been successfully sent.',
+    data: {},
   });
 };
 
@@ -90,7 +92,31 @@ export const resetPasswordController = async (req, res) => {
   await resetPassword(req.body);
   res.json({
     status: 200,
-       message: "Password has been successfully reset.",
-       data: {},
+    message: 'Password has been successfully reset.',
+    data: {},
+  });
+};
+
+export const getGoogleOAuthUrlController = async (req, res) => {
+  const url = generateAuthUrl();
+  res.json({
+    status: 200,
+    message: 'Successfully get Google OAuth url!',
+    data: {
+      url,
+    },
+  });
+};
+
+export const loginWithGoogleController = async (req, res) => {
+  const session = await loginOrSignupWithGoogle(req.body.code);
+  setupSession(res, session);
+
+  res.json({
+    status: 200,
+    message: 'Successfully logged in via Google OAuth!',
+    data: {
+      accessToken: session.accessToken,
+    },
   });
 };
